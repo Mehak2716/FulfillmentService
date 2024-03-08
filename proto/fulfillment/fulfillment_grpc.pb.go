0990bf2389d1c00c15e2443b702595c6ea176fe1
Mehak2716 <mehak.jindal_ftc@external.swiggy.in>
2024-03-08 21:13:02 +0530

    modified proto file for initiate delivery function

--- proto/fulfillment/fulfillment_grpc.pb.go
+++ proto/fulfillment/fulfillment_grpc.pb.go
@@ -18,158 +18,209 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// FulfillmentClient is the client API for Fulfillment service.
+// DeliveryPartnerServiceClient is the client API for DeliveryPartnerService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type FulfillmentClient interface {
-	RegisterDeliveryPartner(ctx context.Context, in *RegisterDeliveryPartnerRequest, opts ...grpc.CallOption) (*DeliveryPartnerResponse, error)
-	GetNearestDeliveryPartner(ctx context.Context, in *Location, opts ...grpc.CallOption) (*NearestDeliveryPartnerResponse, error)
-	UpdateDeliveryStatus(ctx context.Context, in *DeliveryStatus, opts ...grpc.CallOption) (*DeliveryResponse, error)
+type DeliveryPartnerServiceClient interface {
+	RegisterDeliveryPartner(ctx context.Context, in *RegisterDeliveryPartnerRequest, opts ...grpc.CallOption) (*DeliveryPartner, error)
 }
 
-type fulfillmentClient struct {
+type deliveryPartnerServiceClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewFulfillmentClient(cc grpc.ClientConnInterface) FulfillmentClient {
-	return &fulfillmentClient{cc}
+func NewDeliveryPartnerServiceClient(cc grpc.ClientConnInterface) DeliveryPartnerServiceClient {
+	return &deliveryPartnerServiceClient{cc}
 }
 
-func (c *fulfillmentClient) RegisterDeliveryPartner(ctx context.Context, in *RegisterDeliveryPartnerRequest, opts ...grpc.CallOption) (*DeliveryPartnerResponse, error) {
-	out := new(DeliveryPartnerResponse)
-	err := c.cc.Invoke(ctx, "/Fulfillment/RegisterDeliveryPartner", in, out, opts...)
+func (c *deliveryPartnerServiceClient) RegisterDeliveryPartner(ctx context.Context, in *RegisterDeliveryPartnerRequest, opts ...grpc.CallOption) (*DeliveryPartner, error) {
+	out := new(DeliveryPartner)
+	err := c.cc.Invoke(ctx, "/DeliveryPartnerService/RegisterDeliveryPartner", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *fulfillmentClient) GetNearestDeliveryPartner(ctx context.Context, in *Location, opts ...grpc.CallOption) (*NearestDeliveryPartnerResponse, error) {
-	out := new(NearestDeliveryPartnerResponse)
-	err := c.cc.Invoke(ctx, "/Fulfillment/GetNearestDeliveryPartner", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *fulfillmentClient) UpdateDeliveryStatus(ctx context.Context, in *DeliveryStatus, opts ...grpc.CallOption) (*DeliveryResponse, error) {
-	out := new(DeliveryResponse)
-	err := c.cc.Invoke(ctx, "/Fulfillment/UpdateDeliveryStatus", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// FulfillmentServer is the server API for Fulfillment service.
-// All implementations must embed UnimplementedFulfillmentServer
+// DeliveryPartnerServiceServer is the server API for DeliveryPartnerService service.
+// All implementations must embed UnimplementedDeliveryPartnerServiceServer
 // for forward compatibility
-type FulfillmentServer interface {
-	RegisterDeliveryPartner(context.Context, *RegisterDeliveryPartnerRequest) (*DeliveryPartnerResponse, error)
-	GetNearestDeliveryPartner(context.Context, *Location) (*NearestDeliveryPartnerResponse, error)
-	UpdateDeliveryStatus(context.Context, *DeliveryStatus) (*DeliveryResponse, error)
-	mustEmbedUnimplementedFulfillmentServer()
+type DeliveryPartnerServiceServer interface {
+	RegisterDeliveryPartner(context.Context, *RegisterDeliveryPartnerRequest) (*DeliveryPartner, error)
+	mustEmbedUnimplementedDeliveryPartnerServiceServer()
 }
 
-// UnimplementedFulfillmentServer must be embedded to have forward compatible implementations.
-type UnimplementedFulfillmentServer struct {
+// UnimplementedDeliveryPartnerServiceServer must be embedded to have forward compatible implementations.
+type UnimplementedDeliveryPartnerServiceServer struct {
 }
 
-func (UnimplementedFulfillmentServer) RegisterDeliveryPartner(context.Context, *RegisterDeliveryPartnerRequest) (*DeliveryPartnerResponse, error) {
+func (UnimplementedDeliveryPartnerServiceServer) RegisterDeliveryPartner(context.Context, *RegisterDeliveryPartnerRequest) (*DeliveryPartner, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method RegisterDeliveryPartner not implemented")
 }
-func (UnimplementedFulfillmentServer) GetNearestDeliveryPartner(context.Context, *Location) (*NearestDeliveryPartnerResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetNearestDeliveryPartner not implemented")
+func (UnimplementedDeliveryPartnerServiceServer) mustEmbedUnimplementedDeliveryPartnerServiceServer() {
 }
-func (UnimplementedFulfillmentServer) UpdateDeliveryStatus(context.Context, *DeliveryStatus) (*DeliveryResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateDeliveryStatus not implemented")
-}
-func (UnimplementedFulfillmentServer) mustEmbedUnimplementedFulfillmentServer() {}
 
-// UnsafeFulfillmentServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to FulfillmentServer will
+// UnsafeDeliveryPartnerServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to DeliveryPartnerServiceServer will
 // result in compilation errors.
-type UnsafeFulfillmentServer interface {
-	mustEmbedUnimplementedFulfillmentServer()
+type UnsafeDeliveryPartnerServiceServer interface {
+	mustEmbedUnimplementedDeliveryPartnerServiceServer()
 }
 
-func RegisterFulfillmentServer(s grpc.ServiceRegistrar, srv FulfillmentServer) {
-	s.RegisterService(&Fulfillment_ServiceDesc, srv)
+func RegisterDeliveryPartnerServiceServer(s grpc.ServiceRegistrar, srv DeliveryPartnerServiceServer) {
+	s.RegisterService(&DeliveryPartnerService_ServiceDesc, srv)
 }
 
-func _Fulfillment_RegisterDeliveryPartner_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _DeliveryPartnerService_RegisterDeliveryPartner_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(RegisterDeliveryPartnerRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FulfillmentServer).RegisterDeliveryPartner(ctx, in)
+		return srv.(DeliveryPartnerServiceServer).RegisterDeliveryPartner(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/Fulfillment/RegisterDeliveryPartner",
+		FullMethod: "/DeliveryPartnerService/RegisterDeliveryPartner",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FulfillmentServer).RegisterDeliveryPartner(ctx, req.(*RegisterDeliveryPartnerRequest))
+		return srv.(DeliveryPartnerServiceServer).RegisterDeliveryPartner(ctx, req.(*RegisterDeliveryPartnerRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Fulfillment_GetNearestDeliveryPartner_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Location)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(FulfillmentServer).GetNearestDeliveryPartner(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/Fulfillment/GetNearestDeliveryPartner",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FulfillmentServer).GetNearestDeliveryPartner(ctx, req.(*Location))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _Fulfillment_UpdateDeliveryStatus_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(DeliveryStatus)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(FulfillmentServer).UpdateDeliveryStatus(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/Fulfillment/UpdateDeliveryStatus",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FulfillmentServer).UpdateDeliveryStatus(ctx, req.(*DeliveryStatus))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-// Fulfillment_ServiceDesc is the grpc.ServiceDesc for Fulfillment service.
+// DeliveryPartnerService_ServiceDesc is the grpc.ServiceDesc for DeliveryPartnerService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var Fulfillment_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "Fulfillment",
-	HandlerType: (*FulfillmentServer)(nil),
+var DeliveryPartnerService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "DeliveryPartnerService",
+	HandlerType: (*DeliveryPartnerServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "RegisterDeliveryPartner",
-			Handler:    _Fulfillment_RegisterDeliveryPartner_Handler,
+			Handler:    _DeliveryPartnerService_RegisterDeliveryPartner_Handler,
 		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "fulfillment.proto",
+}
+
+// DeliveryServiceClient is the client API for DeliveryService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type DeliveryServiceClient interface {
+	InitiateDelivery(ctx context.Context, in *DeliveryRequest, opts ...grpc.CallOption) (*Response, error)
+	UpdateDeliveryStatus(ctx context.Context, in *DeliveryStatusRequest, opts ...grpc.CallOption) (*Response, error)
+}
+
+type deliveryServiceClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewDeliveryServiceClient(cc grpc.ClientConnInterface) DeliveryServiceClient {
+	return &deliveryServiceClient{cc}
+}
+
+func (c *deliveryServiceClient) InitiateDelivery(ctx context.Context, in *DeliveryRequest, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
+	err := c.cc.Invoke(ctx, "/DeliveryService/initiateDelivery", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *deliveryServiceClient) UpdateDeliveryStatus(ctx context.Context, in *DeliveryStatusRequest, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
+	err := c.cc.Invoke(ctx, "/DeliveryService/UpdateDeliveryStatus", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// DeliveryServiceServer is the server API for DeliveryService service.
+// All implementations must embed UnimplementedDeliveryServiceServer
+// for forward compatibility
+type DeliveryServiceServer interface {
+	InitiateDelivery(context.Context, *DeliveryRequest) (*Response, error)
+	UpdateDeliveryStatus(context.Context, *DeliveryStatusRequest) (*Response, error)
+	mustEmbedUnimplementedDeliveryServiceServer()
+}
+
+// UnimplementedDeliveryServiceServer must be embedded to have forward compatible implementations.
+type UnimplementedDeliveryServiceServer struct {
+}
+
+func (UnimplementedDeliveryServiceServer) InitiateDelivery(context.Context, *DeliveryRequest) (*Response, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method InitiateDelivery not implemented")
+}
+func (UnimplementedDeliveryServiceServer) UpdateDeliveryStatus(context.Context, *DeliveryStatusRequest) (*Response, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateDeliveryStatus not implemented")
+}
+func (UnimplementedDeliveryServiceServer) mustEmbedUnimplementedDeliveryServiceServer() {}
+
+// UnsafeDeliveryServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to DeliveryServiceServer will
+// result in compilation errors.
+type UnsafeDeliveryServiceServer interface {
+	mustEmbedUnimplementedDeliveryServiceServer()
+}
+
+func RegisterDeliveryServiceServer(s grpc.ServiceRegistrar, srv DeliveryServiceServer) {
+	s.RegisterService(&DeliveryService_ServiceDesc, srv)
+}
+
+func _DeliveryService_InitiateDelivery_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeliveryRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DeliveryServiceServer).InitiateDelivery(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/DeliveryService/initiateDelivery",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DeliveryServiceServer).InitiateDelivery(ctx, req.(*DeliveryRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _DeliveryService_UpdateDeliveryStatus_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeliveryStatusRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DeliveryServiceServer).UpdateDeliveryStatus(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/DeliveryService/UpdateDeliveryStatus",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DeliveryServiceServer).UpdateDeliveryStatus(ctx, req.(*DeliveryStatusRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// DeliveryService_ServiceDesc is the grpc.ServiceDesc for DeliveryService service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var DeliveryService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "DeliveryService",
+	HandlerType: (*DeliveryServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "GetNearestDeliveryPartner",
-			Handler:    _Fulfillment_GetNearestDeliveryPartner_Handler,
+			MethodName: "initiateDelivery",
+			Handler:    _DeliveryService_InitiateDelivery_Handler,
 		},
 		{
 			MethodName: "UpdateDeliveryStatus",
-			Handler:    _Fulfillment_UpdateDeliveryStatus_Handler,
+			Handler:    _DeliveryService_UpdateDeliveryStatus_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
